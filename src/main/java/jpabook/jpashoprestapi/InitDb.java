5607cf643617733f519bc72bacbc2fb3ef640fff
yuntaeLee <leeyt1201@gmail.com>
2022-07-04 18:34:58 +0900

    Add Init db

--- src/main/java/jpabook/jpashoprestapi/InitDb.java
+++ src/main/java/jpabook/jpashoprestapi/InitDb.java
@@ -28,6 +28,7 @@ public class InitDb {
     static class InitService {
 
         private final EntityManager em;
+        
         public void dbInit1() {
             Member member = createMember("userA", "서울", "1", "1111");
             em.persist(member);
